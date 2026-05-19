436db6674868bbcc76e2572b3afbba07d6dbe2ac
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-19 15:14:39 +0200

    empty classes created,

---
+++ ChallengeClasses.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""" This file contains 4 classes for the challenge"""
+
+
+class Manager:
+    pass
+
+
+class PatientRecord:
+    """"SuperClass"""
+    pass
+
+
+class ICUPatien(PatientRecord):
+    pass
+
+
+class Outpatient(PatientRecord):
+    pass
---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from ChallengeClasses import *
+
+
+def main():
+
+    manager = Manager()
+    print("empty main")
+
+
+if __name__ == "__main__":
+    main()
