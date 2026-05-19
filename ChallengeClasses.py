1475a005f86c30241f1974a3212d085471a91c87
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-19 15:37:30 +0200

    before splitting the class file

--- ChallengeClasses.py
+++ ChallengeClasses.py
@@ -2,18 +2,28 @@
 """" This file contains 4 classes for the challenge"""
 
 
-class Manager:
-    pass
-
-
 class PatientRecord:
     """"SuperClass"""
     pass
 
 
-class ICUPatien(PatientRecord):
+class ICUPatient(PatientRecord):
     pass
 
 
 class Outpatient(PatientRecord):
     pass
+
+
+class Manager:
+    """Clinic Manager, admits patients, checks medical metrics"""
+
+    def __init__(self):
+        """New manager is born, list of patients is empty"""
+        self.patients = []
+        print("Manager created")
+
+    def admit_patient(self, patient=PatientRecord):
+        """Adds the patient to the list"""
+        self.patients.append(PatientRecord)
+        print("Patient admited")
--- main.py
+++ main.py
@@ -5,7 +5,15 @@ from ChallengeClasses import *
 def main():
 
     manager = Manager()
-    print("empty main")
+    icu_patient = ICUPatient()
+    out_patient = Outpatient()
+    patient = PatientRecord()
+
+    manager.admit_patient(icu_patient)
+    manager.admit_patient(out_patient)
+    manager.admit_patient(patient)
+
+    print(manager.__dict__)
 
 
 if __name__ == "__main__":
